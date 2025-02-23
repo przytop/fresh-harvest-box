cd35780b9a7dc46f63096c7bc4a620b311849723
Pawel Przytomski <przytop@gmail.com>
2025-02-23 23:55:37 +0100

    Update dependencies, improve design, and optimize performance
    - Updated `modern-normalize` to version 3.0.1.
    - Updated various project dependencies to the latest versions.
    - Made several design improvements.
    - Added and optimized JavaScript scripts for better performance.
    - Implemented smoother scrolling functionality.
    - Replaced `fresh-harvest-box.gif` with a more optimized version.

--- src/partials/script.js
+++ src/partials/script.js
@@ -1,13 +1,35 @@
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
-const modalYourOrder = document.querySelector('#modal-yourorder');
+const offScreenMenuLinks = document.querySelectorAll('.off-screen-menu-link');
+const contactForm = document.querySelector('.contacts-form');
+const modalYourOrder = document.querySelector('.modal-yourorder');
+const modalYourOrderClose = document.querySelector('.modal-button-close');
+const modalYourOrderForm = document.querySelector('.modal-form');
+const modalYourOrderSubmit = document.querySelector('#modal-btn-submit');
 const modalThankYou = document.querySelector('#modal-thankyou');
+const modalThankYouBtn = document.querySelector('#thankyou-btn');
 
 // HEADER
-
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
+  if (offScreenMenu.classList.contains('active')) {
+    bodyScrollLock.disableBodyScroll(document.body);
+  } else {
+    bodyScrollLock.enableBodyScroll(document.body);
+  }
+});
+
+offScreenMenuLinks.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href');
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.remove('active');
+    bodyScrollLock.enableBodyScroll(document.body);
+    history.pushState(null, null, window.location.href.split('#')[0]);
+    window.location.hash = targetId;
+  });
 });
 
 document.querySelector('#header-basket').addEventListener('click', () => {
@@ -22,40 +44,88 @@ document.querySelector('#header-basket-white').addEventListener('click', () => {
 
 // FRUITS
 document.querySelector('#order-btn-click').addEventListener('click', () => {
-  modalYourOrder.classList.remove('is-hidden');
-  bodyScrollLock.disableBodyScroll(document.body);
+  setTimeout(() => {
+    modalYourOrder.classList.remove('is-hidden');
+    bodyScrollLock.disableBodyScroll(document.body);
+  }, 500);
 });
 
 // CONTACTS
-document.querySelector('#contacts-modal-button').addEventListener('click', () => {
-  modalThankYou.classList.remove('is-hidden');
+contactForm.addEventListener('submit', event => {
+  event.preventDefault();
+  alert('Subscription successful!');
+  contactForm.reset();
 });
 
 // YOUR ORDER
-document.querySelector('#modal-btn-close').addEventListener('click', () => {
+modalYourOrderClose.addEventListener('click', () => {
   modalYourOrder.classList.add('is-hidden');
-  bodyScrollLock.enableBodyScroll(document.body);
+  if (!offScreenMenu.classList.contains('active')) {
+    bodyScrollLock.enableBodyScroll(document.body);
+  }
 });
 
-document.querySelector('#checkbox-input-one').addEventListener('click', () => {
-  document.querySelector('#checkbox-icon-one').classList.toggle('is-hidden');
+modalYourOrder.addEventListener('click', event => {
+  if (event.target === modalYourOrder) {
+    modalYourOrder.classList.add('is-hidden');
+    if (!offScreenMenu.classList.contains('active')) {
+      bodyScrollLock.enableBodyScroll(document.body);
+    }
+  }
 });
 
-document.querySelector('#checkbox-input-two').addEventListener('click', () => {
-  document.querySelector('#checkbox-icon-two').classList.toggle('is-hidden');
+document.querySelector('.basket.first').addEventListener('click', () => {
+  const checkbox = document.querySelector('.checkbox-input-first');
+  const icon = document.querySelector('#checkbox-icon-one');
+  checkbox.checked = !checkbox.checked;
+  icon.classList.toggle('is-hidden', !checkbox.checked);
 });
 
-document.querySelector('#checkbox-input-three').addEventListener('click', () => {
-  document.querySelector('#checkbox-icon-three').classList.toggle('is-hidden');
+document.querySelector('.basket.second').addEventListener('click', () => {
+  const checkbox = document.querySelector('.checkbox-input-second');
+  const icon = document.querySelector('#checkbox-icon-two');
+  checkbox.checked = !checkbox.checked;
+  icon.classList.toggle('is-hidden', !checkbox.checked);
 });
 
-document.querySelector('#modal-btn-submit').addEventListener('click', () => {
-  modalYourOrder.classList.add('is-hidden');
-  modalThankYou.classList.remove('is-hidden');
+document.querySelector('.basket.third').addEventListener('click', () => {
+  const checkbox = document.querySelector('.checkbox-input-third');
+  const icon = document.querySelector('#checkbox-icon-three');
+  checkbox.checked = !checkbox.checked;
+  icon.classList.toggle('is-hidden', !checkbox.checked);
+});
+
+modalYourOrderForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const checkboxes = document.querySelectorAll(
+    '.checkbox-input-first, .checkbox-input-second, .checkbox-input-third',
+  );
+  const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+  const icons = document.querySelectorAll(
+    '#checkbox-icon-one, #checkbox-icon-two, #checkbox-icon-three',
+  );
+
+  if (isChecked) {
+    modalYourOrder.classList.add('is-hidden');
+    modalThankYou.classList.remove('is-hidden');
+    modalYourOrderForm.reset();
+    checkboxes.forEach(checkbox => (checkbox.checked = false));
+    icons.forEach(icon => icon.classList.add('is-hidden'));
+  } else {
+    alert('Please select at least one fruit!');
+  }
 });
 
 // THANK YOU
-document.querySelector('#thankyou-btn').addEventListener('click', () => {
+modalThankYouBtn.addEventListener('click', () => {
   modalThankYou.classList.add('is-hidden');
   bodyScrollLock.enableBodyScroll(document.body);
+});
+
+modalThankYou.addEventListener('click', event => {
+  if (event.target === modalThankYou) {
+    modalThankYou.classList.add('is-hidden');
+    bodyScrollLock.enableBodyScroll(document.body);
+  }
 });

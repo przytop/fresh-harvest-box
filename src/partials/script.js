bc8713d0804dc14d65e5def47815c7f7fbd3ad85
Pawel Przytomski <przytop@gmail.com>
2025-03-08 22:47:26 +0100

    Update scripts
    Refactored scripts by organizing code into functions for better readability

--- src/partials/script.js
+++ src/partials/script.js
@@ -1,6 +1,9 @@
 const hamMenu = document.querySelector('.ham-menu');
+const headerBasketIcon = document.querySelector('#header-basket');
+const headerBasketIconWhite = document.querySelector('#header-basket-white');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 const offScreenMenuLinks = document.querySelectorAll('.off-screen-menu-link');
+const orderButton = document.querySelector('#order-btn-click');
 const contactForm = document.querySelector('.contacts-form');
 const modalYourOrder = document.querySelector('.modal-yourorder');
 const modalYourOrderClose = document.querySelector('.modal-button-close');
@@ -9,14 +12,44 @@ const modalYourOrderSubmit = document.querySelector('#modal-btn-submit');
 const modalThankYou = document.querySelector('#modal-thankyou');
 const modalThankYouBtn = document.querySelector('#thankyou-btn');
 
-// HEADER
-hamMenu.addEventListener('click', () => {
+const toggleHamMenu = () => {
   hamMenu.classList.toggle('active');
+};
+
+const toggleOffScreenMenu = () => {
   offScreenMenu.classList.toggle('active');
+};
+
+const toggleModalYourOrder = () => {
+  modalYourOrder.classList.toggle('is-hidden');
+};
+
+const toggleBasketCheckbox = (selectorCheckbox, selectorIcon) => {
+  const checkbox = document.querySelector(selectorCheckbox);
+  const icon = document.querySelector(selectorIcon);
+  checkbox.checked = !checkbox.checked;
+  icon.classList.toggle('is-hidden', !checkbox.checked);
+};
+
+const toggleModalThankYou = () => {
+  modalThankYou.classList.toggle('is-hidden');
+};
+
+const enableBodyScroll = () => {
+  bodyScrollLock.enableBodyScroll(document.body);
+};
+
+const disableBodyScroll = () => {
+  bodyScrollLock.disableBodyScroll(document.body);
+};
+
+hamMenu.addEventListener('click', () => {
+  toggleHamMenu();
+  toggleOffScreenMenu();
   if (offScreenMenu.classList.contains('active')) {
-    bodyScrollLock.disableBodyScroll(document.body);
+    disableBodyScroll();
   } else {
-    bodyScrollLock.enableBodyScroll(document.body);
+    enableBodyScroll();
   }
 });
 
@@ -24,91 +57,72 @@ offScreenMenuLinks.forEach(link => {
   link.addEventListener('click', function (event) {
     event.preventDefault();
     const targetId = this.getAttribute('href');
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.remove('active');
-    bodyScrollLock.enableBodyScroll(document.body);
+    toggleHamMenu();
+    toggleOffScreenMenu();
+    enableBodyScroll();
     history.pushState(null, null, window.location.href.split('#')[0]);
     window.location.hash = targetId;
   });
 });
 
-document.querySelector('#header-basket').addEventListener('click', () => {
-  modalYourOrder.classList.remove('is-hidden');
-  bodyScrollLock.disableBodyScroll(document.body);
+headerBasketIcon.addEventListener('click', () => {
+  toggleModalYourOrder();
+  disableBodyScroll();
 });
 
-document.querySelector('#header-basket-white').addEventListener('click', () => {
-  modalYourOrder.classList.remove('is-hidden');
-  bodyScrollLock.disableBodyScroll(document.body);
+headerBasketIconWhite.addEventListener('click', () => {
+  toggleModalYourOrder();
+  disableBodyScroll();
 });
 
-// FRUITS
-document.querySelector('#order-btn-click').addEventListener('click', () => {
+orderButton.addEventListener('click', () => {
   setTimeout(() => {
-    modalYourOrder.classList.remove('is-hidden');
-    bodyScrollLock.disableBodyScroll(document.body);
+    toggleModalYourOrder();
+    disableBodyScroll();
   }, 500);
 });
 
-// CONTACTS
 contactForm.addEventListener('submit', event => {
   event.preventDefault();
   alert('Subscription successful!');
   contactForm.reset();
 });
 
-// YOUR ORDER
 modalYourOrderClose.addEventListener('click', () => {
-  modalYourOrder.classList.add('is-hidden');
+  toggleModalYourOrder();
   if (!offScreenMenu.classList.contains('active')) {
-    bodyScrollLock.enableBodyScroll(document.body);
+    enableBodyScroll();
   }
 });
 
 modalYourOrder.addEventListener('click', event => {
   if (event.target === modalYourOrder) {
-    modalYourOrder.classList.add('is-hidden');
+    toggleModalYourOrder();
     if (!offScreenMenu.classList.contains('active')) {
-      bodyScrollLock.enableBodyScroll(document.body);
+      enableBodyScroll();
     }
   }
 });
 
-document.querySelector('.basket.first').addEventListener('click', () => {
-  const checkbox = document.querySelector('.checkbox-input-first');
-  const icon = document.querySelector('#checkbox-icon-one');
-  checkbox.checked = !checkbox.checked;
-  icon.classList.toggle('is-hidden', !checkbox.checked);
-});
-
-document.querySelector('.basket.second').addEventListener('click', () => {
-  const checkbox = document.querySelector('.checkbox-input-second');
-  const icon = document.querySelector('#checkbox-icon-two');
-  checkbox.checked = !checkbox.checked;
-  icon.classList.toggle('is-hidden', !checkbox.checked);
-});
-
-document.querySelector('.basket.third').addEventListener('click', () => {
-  const checkbox = document.querySelector('.checkbox-input-third');
-  const icon = document.querySelector('#checkbox-icon-three');
-  checkbox.checked = !checkbox.checked;
-  icon.classList.toggle('is-hidden', !checkbox.checked);
+document.querySelectorAll('.basket').forEach(basket => {
+  basket.addEventListener('click', () => {
+    const index = basket.dataset.index;
+    toggleBasketCheckbox(`.checkbox-input-${index}`, `#checkbox-icon-${index}`);
+  });
 });
 
 modalYourOrderForm.addEventListener('submit', event => {
   event.preventDefault();
-
   const checkboxes = document.querySelectorAll(
     '.checkbox-input-first, .checkbox-input-second, .checkbox-input-third',
   );
   const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
   const icons = document.querySelectorAll(
-    '#checkbox-icon-one, #checkbox-icon-two, #checkbox-icon-three',
+    '#checkbox-icon-first, #checkbox-icon-second, #checkbox-icon-third',
   );
-
   if (isChecked) {
-    modalYourOrder.classList.add('is-hidden');
-    modalThankYou.classList.remove('is-hidden');
+    toggleModalYourOrder();
+    toggleModalThankYou();
     modalYourOrderForm.reset();
     checkboxes.forEach(checkbox => (checkbox.checked = false));
     icons.forEach(icon => icon.classList.add('is-hidden'));
@@ -117,15 +131,14 @@ modalYourOrderForm.addEventListener('submit', event => {
   }
 });
 
-// THANK YOU
 modalThankYouBtn.addEventListener('click', () => {
-  modalThankYou.classList.add('is-hidden');
-  bodyScrollLock.enableBodyScroll(document.body);
+  toggleModalThankYou();
+  enableBodyScroll();
 });
 
 modalThankYou.addEventListener('click', event => {
   if (event.target === modalThankYou) {
-    modalThankYou.classList.add('is-hidden');
-    bodyScrollLock.enableBodyScroll(document.body);
+    toggleModalThankYou();
+    enableBodyScroll();
   }
 });
